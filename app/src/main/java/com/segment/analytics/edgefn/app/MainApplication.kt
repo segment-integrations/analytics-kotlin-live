2fdc481d0f5177db08d5ea00a812a45af06d20ed
Prayansh Srivastava <prayansh@gmail.com>
2022-07-22 14:02:38 -0700

    update sample app

--- app/src/main/java/com/segment/analytics/edgefn/app/MainApplication.kt
+++ app/src/main/java/com/segment/analytics/edgefn/app/MainApplication.kt
@@ -1,10 +1,17 @@
 package com.segment.analytics.edgefn.app
 
 import android.app.Application
+import com.segment.analytics.edgefn.app.filters.DestinationFilters
+import com.segment.analytics.edgefn.app.filters.WebhookPlugin
 import com.segment.analytics.edgefn.kotlin.EdgeFunctions
 import com.segment.analytics.kotlin.android.Analytics
 import com.segment.analytics.kotlin.core.Analytics
-import com.segment.analytics.kotlin.core.platform.plugins.logger.logFlush
+import com.segment.analytics.kotlin.core.manuallyEnableDestination
+import com.segment.analytics.substrata.kotlin.asJSValue
+import com.segment.analytics.substrata.kotlin.j2v8.J2V8Engine
+import kotlinx.serialization.json.buildJsonObject
+import kotlinx.serialization.json.put
+import java.util.concurrent.Executors
 
 class MainApplication : Application() {
     companion object {
@@ -15,7 +22,7 @@ class MainApplication : Application() {
         super.onCreate()
 
         analytics = Analytics(
-            "HO63Z36e0Ufa8AAgbjDomDuKxFuUICqI",
+            "93EMLzmXzP6EJ3cJOhdaAgEVNnZjwRqA",
             applicationContext
         ) {
             this.collectDeviceId = true
@@ -24,8 +31,10 @@ class MainApplication : Application() {
             this.flushAt = 1
             this.flushInterval = 0
         }
+
         val backup = resources.openRawResource(R.raw.default_edgefn)
         analytics.add(EdgeFunctions(backup, true))
         analytics.track("howdy doody")
     }
+
 }
