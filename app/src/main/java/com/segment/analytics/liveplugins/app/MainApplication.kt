e643da82ce42ac6d96ab5805999adfda0509c51b
Wenxi Zeng <wenxi.zeng@outlook.com>
2024-05-03 14:03:51 -0500

    Replace snapshot dependencies (#12)
    * fix ci
    
    * replace snapshot dependencies
    
    * replace toBaseEvent function
    
    * update example usage
    
    ---------
    
    Co-authored-by: Wenxi Zeng <wzeng@twilio.com>

--- app/src/main/java/com/segment/analytics/liveplugins/app/MainApplication.kt
+++ app/src/main/java/com/segment/analytics/liveplugins/app/MainApplication.kt
@@ -3,8 +3,11 @@ package com.segment.analytics.liveplugins.app
 import android.app.Application
 import com.segment.analytics.kotlin.android.Analytics
 import com.segment.analytics.kotlin.core.Analytics
+import com.segment.analytics.kotlin.core.platform.Plugin
 import com.segment.analytics.liveplugins.app.filters.WebhookPlugin
 import com.segment.analytics.liveplugins.kotlin.LivePlugins
+import com.segment.analytics.liveplugins.kotlin.LivePluginsDependent
+import com.segment.analytics.substrata.kotlin.JSScope
 import java.util.concurrent.Executors
 
 class MainApplication : Application() {
@@ -15,7 +18,7 @@ class MainApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         analytics = Analytics(
-            "93EMLzmXzP6EJ3cJOhdaAgEVNnZjwRqA",
+            "KZrBizvzx7GOj3Ek9Gin7GPsYhezAzw6",
             applicationContext
         ) {
             this.collectDeviceId = true
@@ -25,11 +28,29 @@ class MainApplication : Application() {
             this.flushInterval = 0
         }
 
-        analytics.add(WebhookPlugin("https://webhook.site/5fefa55b-b5cf-4bd5-abe6-9234a003baa8", Executors.newSingleThreadExecutor()))
+//        analytics.add(WebhookPlugin("https://webhook.site/5fefa55b-b5cf-4bd5-abe6-9234a003baa8", Executors.newSingleThreadExecutor()))
 
         val backup = resources.openRawResource(R.raw.default_liveplugins)
         val livePlugins = LivePlugins(backup, true)
         analytics.add(livePlugins)
+        analytics.add(Signals)
     }
 
+    object Signals: Plugin, LivePluginsDependent {
+        override lateinit var analytics: Analytics
+        override val type: Plugin.Type = Plugin.Type.After
+        override fun setup(analytics: Analytics) {
+            super.setup(analytics)
+
+            analytics.find(LivePlugins::class)?.addDependent(this)
+        }
+        override fun prepare(engine: JSScope) {
+            println("prepare called")
+        }
+
+        override fun readyToStart() {
+            println("readyToStart called")
+        }
+
+    }
 }
